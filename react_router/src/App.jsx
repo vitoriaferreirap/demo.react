55d7b66b3c222190b5a7e7be1cee84e833b953c4
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-08-06 14:58:09 -0300

    feat: criando novas rotas e novas paginas

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import "./App.css";
+import About from './components/About';
+import Contact from './components/Contact';
 import Home from './components/Home';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
 
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
 
         </>
