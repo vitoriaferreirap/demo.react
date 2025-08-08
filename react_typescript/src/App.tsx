febeb6b564a5a45bfe086b445f11d8f33b5282d2
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-08-08 11:01:17 -0300

    feat: criando projeto usando react + typescript

---
+++ react_typescript/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <h1>React + TS</h1>
+            {/* componentes funcionais com TS*/}
+        </>
+    )
+}
+
+export default App
